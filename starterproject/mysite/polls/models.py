c3ad5ae96eaa42fbfef65054381732a57187d550
bek000han <bekuniversity@gmail.com>
2024-07-19 15:26:07 +0500

    renamed models

--- starterproject/mysite/polls/models.py
+++ starterproject/mysite/polls/models.py
@@ -5,19 +5,19 @@ from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
-    questionText = models.CharField(max_length=200)
-    pubDate = models.DateTimeField("date published")
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
 
     def __str__(self):
         return self.questionText
     
-    def wasPublishedRecently(self):
-        return self.pubDate >= timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choiceText = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choiceText
+        return self.choice_text
